0ff2fe1ef1a7483a01c093ada338001b06a060a2
annt154 <nhduong1978@gmail.com>
2025-09-26 20:53:21 +1000

    dr

---
+++ assignment2/data.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "DryDown",
+    path: "p-hase_Dry-Down-feat-Ben-Snaath.mp3",
+    artist: "p-hase ft. Ben Snaath",
+    cover: "1.jpg",
+  },
+  {
+    name: "Hes",
+    path: "p-hase_Hes.mp3",
+    artist: "p-hase",
+    cover: "4.jpg",
+  },
+  {
+    name: "Leapt",
+    path: "p-hase_Leapt.mp3",
+    artist: "p-hase",
+    cover: "5.jpg",
+  },
+  {
+    name: "Water Feature",
+    path: "p-hase_Water-Feature.mp3",
+    artist: "p-hase",
+    cover: "6.jpg",
+  },
+];
--- assignment2/script.js
+++ assignment2/script.js
@@ -1,21 +1,96 @@
-const video = document.querySelector("#custom-video-player");
-const playPauseBtn = document.querySelector("#play-pause-btn");
-const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
-video.removeAttribute("controls");
-// playPauseBtn.addEventListener("click", togglePlayPause);
-video.addEventListener("timeupdate", updateProgressBar);
-function togglePlayPause() {
-  if (video.paused || video.ended) {
-    video.play();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+let currentMusic = 0;
+
+const music = document.querySelector("#audio");
+
+const seekBar = document.querySelector(".seek-bar");
+const songName = document.querySelector(".music-name");
+const artistName = document.querySelector(".artist-name");
+const disk = document.querySelector(".disk");
+const currentTime = document.querySelector(".current-time");
+const musicDuration = document.querySelector(".song-duration");
+const playButton = document.querySelector(".play-button");
+const forwardButton = document.querySelector(".forward-button");
+const backwardButton = document.querySelector(".backward-button");
+
+playButton.addEventListener("click", () => {
+  if (playButton.className.includes("pause")) {
+    music.play();
   } else {
-    video.pause();
-    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+    music.pause();
   }
-}
-function updateProgressBar() {
-  const value = (video.currentTime / video.duration) * 100;
-  progressBar.style.width = value + "%";
-}
-// Add other functionalities here
+  playButton.classList.toggle("pause");
+  disk.classList.toggle("play");
+});
+
+// music display
+
+const setMusic = (i) => {
+  seekBar.value = 0; //set range slide value
+  let song = songs[i];
+  currentMusic = i;
+  music.src = song.path;
+
+  songName.innerHTML = song.name;
+  artistName.innerHTML = song.artist;
+  disk.style.backgroundImage = `url('${song.cover}')`;
+
+  currentTime.innerHTML = "00:00";
+  setTimeout(() => {
+    seekBar.max = music.duration;
+    musicDuration.innerHTML = formatTime(music.duration);
+  }, 300);
+};
+
+setMusic(0);
+
+const formatTime = (time) => {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+};
+
+//seek bar running
+setInterval(() => {
+  seekBar.value = music.currentTime;
+  currentTime.innerHTML = formatTime(music.currentTime);
+  if (Math.floor(music.currentTime) == Math.floor(seekBar.max)) {
+    forwardButton.click();
+  }
+}, 500);
+
+seekBar.addEventListener("change", () => {
+  music.currentTime = seekBar.value;
+});
+
+const playMusic = () => {
+  music.play();
+  playButton.classList.remove("pause");
+  disk.classList.add("play");
+};
+
+//changing song function
+forwardButton.addEventListener("click", () => {
+  if (currentMusic >= songs.length - 1) {
+    currentMusic = 0;
+  } else {
+    currentMusic++;
+  }
+  setMusic(currentMusic);
+  playMusic();
+});
+
+backwardButton.addEventListener("click", () => {
+  if (currentMusic <= 0) {
+    currentMusic = songs.length - 1;
+  } else {
+    currentMusic--;
+  }
+  setMusic(currentMusic);
+  playMusic();
+});
